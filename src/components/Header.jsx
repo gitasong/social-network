09e1cda15eda49a649393d8c4774b3c747757b3b
Calla Rudolph and Nicole Freed <calla@>
2017-09-11 11:35:55 -0700

    style Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 
 function Header(props) {
+  var listStyles = {
+    display: "inline-block"
+  }
+  var linkStyles = {
+    display: "inline-block",
+    color: "blue",
+    margin: ".5rem .5rem 0 .5rem",
+    padding: "1rem 1rem 0 1rem",
+  }
+  var inputStyles = {
+    float: "right",
+    margin: "0 1rem 1rem 1rem",
+    padding: ".5rem",
+  }
   return (
     <div>
-      <ul>
-        <li>Home</li>
-        <li>Notifications</li>
-        <li>Messages</li>
+      <ul style={listStyles}>
+        <li style={linkStyles}>Home</li>
+        <li style={linkStyles}>Notifications</li>
+        <li style={linkStyles}>Messages</li>
       </ul>
-      <input placeholder="Search"></input>
-      <input placeholder="Tweet"></input>
+      <input style={inputStyles} placeholder="Search"></input>
+      <input style={inputStyles} placeholder="Tweet"></input>
+      <hr/>
     </div>
   );
 }
