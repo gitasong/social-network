b41cc0ca2ce63b3705f1bf68060c6dd5c0161baf
Calla Rudolph and Nicole Freed <nicole@>
2017-09-11 10:56:54 -0700

    import RecommendedEntry; add URL to avatar

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import RecommendedEntry from "./RecommendedEntry";
 
 function Recommended(props) {
   return (
-    <RecommendedEntry
-      avatar="#"
-      title="Some Twitter List"
-    />
-);
+    <div>
+      <h2>Recommended to Follow</h2>
+      <RecommendedEntry
+        avatar="https://image.flaticon.com/icons/png/512/78/78373.png"
+        title="Some Twitter List"
+      />
+    </div>
+  );
 }
 
 export default Recommended;
