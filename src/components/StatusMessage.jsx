72eaf2b25d97f9c97103f78fd66b9feb15d046e2
Calla Rudolph and Nicole Freed <nicole@>
2017-09-11 09:36:53 -0700

    create StatusMessage component with dummy data

---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function StatusMessage(props) {
+  return (
+    <div>
+      <h3>Jimmy</h3>
+      <p>Hello. Goodbye.</p>
+    </div>
+  );
+}
+
+export default StatusMessage;
