52d040bb11a0d88ce2ce8de43f34cb0a2e1a628a
Calla Rudolph and Nicole Freed <calla+nicole@>
2017-09-11 09:54:47 -0700

    update webpack config entry, output, plugins, devtool, and devServer

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,28 @@ const { resolve } = require('path');
 module.exports = {
 
   entry: [
-    resolve(__dirname, "src") + "/index.jsx"
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    resolve(__dirname, "src", "index.jsx")
   ],
 
   output: {
     filename: 'app.bundle.js',
     path: resolve(__dirname, 'build'),
+    publicPath: '/'
   },
 
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+
+  devtool: '#source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'build'),
+    publicPath: '/'
   },
 
   module: {
@@ -26,9 +38,17 @@ module.exports = {
           presets: [
             "es2015",
             "react"
+          ],
+          plugins: [
+            "react-hot-loader/babel"
           ]
         }
       },
     ],
-  }
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ]
 };
