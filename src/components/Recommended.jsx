7bcdc961bc8675849874e4090d6b89cc73ee99c6
Calla Rudolph and Nicole Freed <nicole@>
2017-09-11 10:35:03 -0700

    create Recommended component w/dummy data

---
+++ src/components/Recommended.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Recommended(props) {
+  return (
+    <RecommendedEntry
+      avatar="#"
+      title="Some Twitter List"
+    />
+);
+}
+
+export default Recommended;
