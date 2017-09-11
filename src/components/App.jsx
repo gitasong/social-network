eedc991d341b9736dbd29f5883f9dafbc3f174f2
Calla Rudolph and Nicole Freed <nicole@>
2017-09-11 09:38:38 -0700

    import StatusMessage into Feed; import Feed into App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Profile from "./Profile";
+import Feed from "./Feed";
 
 function App(props) {
   return (
     <div>
       <Profile/>
+      <Feed />
     </div>
   );
 }
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import StatusMessage from "./StatusMessage";
 
 function Feed(props) {
-  return ();
+  return (
+    <StatusMessage />
+  );
 }
 
 export default Feed;
